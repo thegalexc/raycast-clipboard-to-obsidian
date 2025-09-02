26739702e4d4ade2db733cafef5fb1578d2c2ff0
Gilman Callsen <gilman.callsen@rhoimpact.com>
2025-09-02 15:37:42 -0400

    Initial commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Obsidian Vault Path - Path to your Obsidian vault */
+  "vaultPath": string,
+  /** Notes Subfolder - Subfolder within vault for new notes */
+  "notesSubfolder": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `create-note` command */
+  export type CreateNote = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `create-note` command */
+  export type CreateNote = {}
+}
+
